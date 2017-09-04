3f7905a6b331e57cb5b29809b82a28ee817f8397
robinson <robincoello@hotmail.com>
2017-09-04 16:59:07 +0200

    add logo

---
+++ admin/footer.php
@@ -0,0 +1,36 @@
+     
+    </div>
+</div>
+
+		  </div>
+            
+		</div>
+    </div>
+
+    <footer>
+         <div class="container">
+         
+            <div class="copy text-center">
+               Copyright 2014 <a href='#'>Website</a>
+            </div>
+            
+         </div>
+      </footer>
+
+      <link href="../temas/admin3/vendors/datatables/dataTables.bootstrap.css" rel="stylesheet" media="screen">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- jQuery UI -->
+    <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../temas/admin3/admin/bootstrap/js/bootstrap.min.js"></script>
+
+    <script src="../temas/admin3/vendors/datatables/js/jquery.dataTables.min.js"></script>
+
+    <script src="../temas/admin3/vendors/datatables/dataTables.bootstrap.js"></script>
+
+    <script src="../temas/admin3/admin/js/custom.js"></script>
+    <script src="../temas/admin3/admin/js/tables.js"></script>
+  </body>
+</html>
